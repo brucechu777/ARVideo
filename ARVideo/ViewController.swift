09221e98f6a6b02d9f8441507a4bbbcc1cb9f5d4
bruce <bruce@bruce-6.local>
2017-12-12 23:40:55 +0800

    修改代码

--- ARVideo/ViewController.swift
+++ ARVideo/ViewController.swift
@@ -10,13 +10,35 @@ import UIKit
 import SceneKit
 import ARKit
 import AVFoundation
+import ARVideoKit
+import Photos
 
-class ViewController: UIViewController, ARSCNViewDelegate {
+class ViewController: UIViewController, ARSCNViewDelegate, RecordARDelegate {
+    func recorder(didEndRecording path: URL, with noError: Bool) {
+        
+    }
+    
+    func recorder(didFailRecording error: Error?, and status: String) {
+        
+    }
+    
+    func recorder(willEnterBackground status: RecordARStatus) {
+        if status == .recording {
+            recorder?.stopAndExport()
+        }
+    }
+    
 
     @IBOutlet var sceneView: ARSCNView!
     
-    // 音乐播放器
+    // 录像按钮
+    @IBOutlet weak var recordBtn: UIButton!
     
+    // 录像
+    var recorder:RecordAR?
+    let recordingQueue = DispatchQueue(label: "recordingThread", attributes: .concurrent)
+    let caprturingQueue = DispatchQueue(label: "capturingThread", attributes: .concurrent)
+    // 音乐播放器
     private var audioPlayer: AVAudioPlayer?
     
     override func viewDidLoad() {
@@ -25,6 +47,17 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Set the view's delegate
         sceneView.delegate = self
         
+        // Initialize ARVideoKit recorder
+        recorder = RecordAR(ARSceneKit: sceneView)
+        
+        /*----👇---- ARVideoKit Configuration ----👇----*/
+        
+        // Set the recorder's delegate
+        recorder?.delegate = self
+        
+        recordBtn.setTitle("录像", for: .normal)
+        
+        // 载入AR
         loadAnimation()
         
         // 载入音乐
@@ -54,6 +87,62 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         audioPlayer!.play()
     }
     
+    
+    // 触发录像
+    @IBAction func record(_ sender: Any) {
+        //Record
+        if recorder?.status == .readyToRecord {
+            recordBtn.setTitle("停止", for: .normal)
+            recordingQueue.async {
+                self.recorder?.record()
+            }
+        }else if recorder?.status == .recording {
+            recordBtn.setTitle("录像", for: .normal)
+            recorder?.stop() { path in
+                self.recorder?.export(video: path) { saved, status in
+                    DispatchQueue.main.sync {
+                        self.exportMessage(success: saved, status: status)
+                    }
+                }
+            }
+        }
+    }
+    
+    // 录像后
+    func exportMessage(success: Bool, status:PHAuthorizationStatus) {
+        if success {
+            let alert = UIAlertController(title: "Exported", message: "Media exported to camera roll successfully!", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Awesome", style: .cancel, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        }else if status == .denied || status == .restricted || status == .notDetermined {
+            let errorView = UIAlertController(title: "😅", message: "Please allow access to the photo library in order to save this media file.", preferredStyle: .alert)
+            let settingsBtn = UIAlertAction(title: "Open Settings", style: .cancel) { (_) -> Void in
+                guard let settingsUrl = URL(string: UIApplicationOpenSettingsURLString) else {
+                    return
+                }
+                if UIApplication.shared.canOpenURL(settingsUrl) {
+                    if #available(iOS 10.0, *) {
+                        UIApplication.shared.open(settingsUrl, completionHandler: { (success) in
+                        })
+                    } else {
+                        UIApplication.shared.openURL(URL(string:UIApplicationOpenSettingsURLString)!)
+                    }
+                }
+            }
+            errorView.addAction(UIAlertAction(title: "Later", style: UIAlertActionStyle.default, handler: {
+                (UIAlertAction)in
+            }))
+            errorView.addAction(settingsBtn)
+            self.present(errorView, animated: true, completion: nil)
+        }else{
+            let alert = UIAlertController(title: "Exporting Failed", message: "There was an error while exporting your media file.", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        }
+    }
+    
+    
+    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
@@ -62,5 +151,8 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Run the view's session
         sceneView.session.run(configuration)
+        
+        // Prepare the recorder with sessions configuration
+        recorder?.prepare(configuration)
     }
 }
